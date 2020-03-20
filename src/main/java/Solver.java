8bd6e256a8e83ee42fd141403f4b1860a1c0280a
FireElementalNE <fireelemental.ne@gmail.com>
2020-03-20 09:56:36 -0400

    Added outline for Solver class

---
+++ src/main/java/Solver.java
@@ -0,0 +1,26 @@
+public class Solver {
+
+    // TODO: this will be the solver class!
+
+    /*
+        I am going to use z3-solver python package (it is a nightmare in java....)
+        1. Write the file
+        2. run the program
+        3. get the results!
+
+        For example turn this:
+
+            i3 (i14_1 - 1) -> i14_1 (Phi(i14, i14_2))
+
+        Into this:
+
+            from z3 import *
+            i14_1 = Int('i14_1')
+            i3 = Int('i3')
+            s = Solver()
+            s.add(i3 == i14_1 - 1)
+            print(s.check())
+            print(s.model())
+
+     */
+}
